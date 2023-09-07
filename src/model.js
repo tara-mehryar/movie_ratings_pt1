b084cd24e5056c430aeef772c941a02d94ac571f
Tara Mehryar <tara.mehryar@gmail.com>
2023-09-07 10:55:23 -0500

    Update testUser and testMovie

--- src/model.js
+++ src/model.js
@@ -1,4 +1,4 @@
-import { Model } from 'sequelize';
+import { Model, DataTypes } from 'sequelize';
 import util from 'util';
 import connectToDB from './db.js';
 
@@ -10,6 +10,39 @@ export class User extends Model {
   }
 }
 
+export class Movie extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+
+Movie.init(
+  {
+    movieId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    overview: {
+      type: DataTypes.TEXT,
+    },
+    releaseDate: {
+      type: DataTypes.DATE,
+    },
+    posterPath: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    modelName: 'movie',
+    sequelize: db,
+  },
+);
+
 User.init(
   {
     userId: {
@@ -17,9 +50,26 @@ User.init(
       autoIncrement: true,
       primaryKey: true,
     },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     modelName: 'user',
     sequelize: db,
   },
 );
+
+await db.sync({ force: true })
+
+const testUser = await User.create({ email: 'test@email.com', password: 'test' });
+const testMovie = await Movie.create ({ title: 'Interstellar' });
+
+console.log(testUser);
+console.log(testMovie);
